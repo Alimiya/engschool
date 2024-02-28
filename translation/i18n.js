5277a19c0d107f9f45c54c825516f7308eb1506f
cnrprod-team-50452 <1690610961@codenrock.com>
2024-02-28 14:24:22 +0600

    added i18n

--- translation/i18n.js
+++ translation/i18n.js
@@ -1,7 +1,43 @@
-const i18n = {
-    en: require('./en.json'),
-    kz: require('./kz.json'),
-    ru: require('./ru.json')
+export const i18nPromises = {
+    en: fetchTranslations('en'),
+    kz: fetchTranslations('kz'),
+    ru: fetchTranslations('ru')
 }
 
-module.exports = i18n
+async function fetchTranslations(language) {
+    const response = await fetch(`/${language}.json`)
+    if (!response.ok) {
+        throw new Error(`Failed to fetch ${language}.json: ${response.status} ${response.statusText}`)
+    }
+    return await response.json()
+}
+
+Promise.all(Object.values(i18nPromises)).then(translations => {
+    console.log(translations)
+    const i18n = {}
+    Object.keys(i18nPromises).forEach((language, index) => {
+        i18n[language] = translations[index]
+    })
+
+    function setLanguage(language) {
+        const elements = document.querySelectorAll('[data-i18n]')
+        elements.forEach(element => {
+            const key = element.getAttribute('data-i18n')
+            element.textContent = i18n[language][key]
+        })
+    }
+
+    const defaultLanguage = 'kz'
+    setLanguage(defaultLanguage)
+
+    const buttons = document.querySelectorAll('.flag-button')
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const language = this.dataset.language
+            setLanguage(language)
+        })
+    })
+
+}).catch(err => {
+    console.log(err)
+})
